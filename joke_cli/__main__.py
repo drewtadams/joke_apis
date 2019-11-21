e4de4a50f1b68aff7d24820d9f6f2ca67db34c5a
Drew Adams <drew@drewtadams.com>
2019-11-20 21:38:19 -0700

    fixed distribution errors

--- joke_cli/__main__.py
+++ joke_cli/__main__.py
@@ -1,12 +1,11 @@
 # joke APIs pulled from https://www.programmableweb.com/category/humor/api
-import joke_apis as j
+import joke_apis
 import sys
 
 
 def main():
-	jokes = j.Jokes(sys.argv)
+	jokes = joke_apis.Jokes(sys.argv)
 	print(f'\n\n{jokes.tell()}\n\n')
-	# print(jokes.icanhazdadjoke())
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-	name = 'joke-cli',
-	version = '0.1.0',
-	packages = 'joke-cli',
-	entry_points = {
-		'console_scripts': [ 'joke-cli = joke-cli.__main__:main' ]
-	})
+	name='joke_cli',
+	version='0.1.0',
+	packages=find_packages(),
+	entry_points={
+		'console_scripts': [ 'joke_cli = joke_cli.__main__:main' ]
+	},
+
+	# metadata
+    author="Drew Adams",
+    author_email="drew@drewtadams.com",
+    description="CLI that fetches and prints random jokes",
+    keywords="random jokes",
+    url="https://github.com/drewtadams/joke_apis"
+)
